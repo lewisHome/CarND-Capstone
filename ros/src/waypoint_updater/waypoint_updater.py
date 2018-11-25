2571f432597da889c0431a86c394212b6d06fc7f
Lewis Luyken <31958005+lewisHome@users.noreply.github.com>
2018-11-25 16:16:48 +1100

    edits to work in classroom
    shorter lookahead_wps
    rospy.refresh.rate(10)

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -13,20 +13,16 @@ import math
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
-
 As mentioned in the doc, you should ideally first implement a version which does not care
 about traffic lights or obstacles.
-
 Once you have created dbw_node, you will update this node to use the status of traffic lights too.
-
 Please note that our simulator also provides the exact location of traffic lights and their
 current status in `/vehicle/traffic_lights` message. You can use this message to build this node
 as well as to verify your TL classifier.
-
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200  # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 100  # Number of waypoints we will publish. You can change this number
 MAX_DECEL = 0.5
 
 
@@ -53,7 +49,7 @@ class WaypointUpdater(object):
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(50)  # can go as low as 30Hz
+        rate = rospy.Rate(10)  # can go as low as 30Hz
         while not rospy.is_shutdown():
             if self.pose and self.base_lane:
                 self.publish_waypoints()
