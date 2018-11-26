9d1ff1312d018ed696a84d51d42f8887961804bb
Lewis Luyken <31958005+lewisHome@users.noreply.github.com>
2018-11-27 01:18:00 +1100

    Update tl_classifier.py

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -33,35 +33,37 @@ class TLClassifier(object):
                 self.detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
                 self.num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
-    def get_classification(self, image):
-        """Determines the color of the traffic light in the image
+def get_classification(self, image):
+       """Determines the color of the traffic light in the image
 
-        Args:
-            image (cv::Mat): image containing the traffic light
+       Args:
+           image (cv::Mat): image containing the traffic light
 
-        Returns:
-            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+       Returns:
+           int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
-        """
-        #h,_,_ = image.shape
-        
-        #image_crop = image[:int(h*0.5),:] 
-        image_np_expanded = np.expand_dims(image, axis=0)
-        
-        ret = self.sess.run(
-              [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
-              feed_dict={self.image_tensor: image_np_expanded})
-        
-        prediction = ret[2][0][0]
-        boxes = ret[0][0][0]
-        ratio = (boxes[2]-boxes[0])/(boxes[3]-boxes[1])
-        score = ret[1][0][0]
-        
-        if prediction == 1 and ratio > 2.0 and score > 0.5:
-            return 2, score, ratio
-        elif prediction == 2 and ratio > 2.0 and score > 0.5:
-            return 0, score, ratio
-        elif prediction == 3 and ratio > 2.0 and score > 0.5:
-            return 1, score, ratio
-        else:
-            return 4, score, ratio
+       """
+       image_np_expanded = np.expand_dims(image, axis=0)
+
+       (boxes,scores,classes,num_detections) = self.sess.run(
+             [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
+             feed_dict={self.image_tensor: image_np_expanded})
+
+#        boxes = np.squeeze(boxes)
+       scores = np.squeeze(scores)
+       classes = np.squeeze(classes)
+
+       prediction = int(np.around(np.median(classes[:3])))
+
+       if prediction == 1:
+           print("GREEN")
+           return 2
+       elif prediction == 2:
+           print("RED")
+           return 0
+       elif prediction == 3:
+           print("YELLOW")
+           return 1
+       else:
+           print("UNKOWN")
+           return 4
